5d0f4c890adf45e6bf91e9465f754132e4df5cb8
Martina <martina.colmenero@nayra.coop>
2020-05-13 18:36:16 -0300

    WIP donations section

--- src/components/Donations.vue
+++ src/components/Donations.vue
@@ -9,33 +9,29 @@
         </b-col>
       </b-row>
       <b-row>
+        <b-col class="col-12 col-md-4">
+          <div class="donation-type">
+            <h4>Donación única (dentro de Argentina)</h4>
+            <b-button>{{ $t('donations.donate') }}</b-button>
+            <b-button>{{ $t('donations.donate') }}</b-button>
+            <b-button>{{ $t('donations.donate') }}</b-button>
+          </div>
+          <div class="donation-type">
+            <h4>Donación única (dentro de Argentina)</h4>
+            <b-button>{{ $t('donations.donate') }}</b-button>
+            <b-button>{{ $t('donations.donate') }}</b-button>
+            <b-button>{{ $t('donations.donate') }}</b-button>
+          </div>
+          <div class="donation-type">
+            <h4>Donar desde el exterior de Argentina</h4>
+            <b-button>{{ $t('donations.donate') }}</b-button>
+          </div>
+        </b-col>   
          <b-col class="col-12 col-md-8">
           <p>{{ $t('donations.content[0]') }}</p>
           <p>{{ $t('donations.content[1]') }}</p>
           <p>{{ $t('donations.content[2]') }}</p>
          </b-col>
-      </b-row>
-      <b-row>
-        <b-col class="col-12 col-md-6 mb-2 donation-type">
-        <h4>Donación única (dentro de Argentina)</h4>
-        <b-button>{{ $t('donations.donate') }}</b-button>
-        <b-button>{{ $t('donations.donate') }}</b-button>
-        <b-button>{{ $t('donations.donate') }}</b-button>
-        </b-col>        
-      </b-row>
-      <b-row>
-        <b-col class="col-12 col-md-6 mb-2 donation-type">
-          <h4>Suscripción a débito automático (dentro de Argentina)</h4>
-          <b-button>{{ $t('donations.donate') }}</b-button>
-          <b-button>{{ $t('donations.donate') }}</b-button>
-          <b-button>{{ $t('donations.donate') }}</b-button>
-        </b-col>
-      </b-row>
-      <b-row>
-        <b-col class="col-12 col-md-6 mb-2 donation-type">
-          <h4>Donar desde el exterior de Argentina</h4>
-          <b-button>{{ $t('donations.donate') }}</b-button>
-        </b-col>
       </b-row>
     </b-container>
   </section>
