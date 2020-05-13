fd96428834ff2fe4c7b159c1b64951ab6d9bc429
Martina <martina.colmenero@nayra.coop>
2020-05-13 11:31:59 -0300

    mas contenidos y estilos

--- src/components/About.vue
+++ src/components/About.vue
@@ -2,11 +2,66 @@
   <section id="about">
     <b-container>
       <b-row class="about-info">
-        <b-col class="col-12 col-md-6 mb-5">
+        <b-col class="col-12 col-md-6 mb-1">
           <h2 class="pb-2">
             {{ $t('about.title') }}
           </h2>
-          <p>{{ $t('about.content') }}</p>
+        </b-col>
+      </b-row>
+       <b-row class="about-info">
+        <b-col class="col-12 col-md-6 mb-1">
+          <h4 class="pb-2">
+            {{ $t('about.content[0].subtitle') }}
+          </h4>
+          <p>{{ $t('about.content[0].text') }}</p>
+        </b-col>
+      </b-row>
+      <b-row class="about-info">
+        <b-col class="col-12 col-md-8 mb-1">
+          <h4 class="pb-2">
+            {{ $t('about.content[1].subtitle') }}
+          </h4>
+          <p>{{ $t('about.content[1].text') }}</p>
+        </b-col>
+      </b-row>
+      <b-row class="about-info">
+        <b-col class="col-12 col-md-6 mb-1">
+          <h4 class="pb-2">
+            {{ $t('about.content[2].subtitle') }}
+          </h4>
+          <p>{{ $t('about.content[2].text') }}</p>
+        </b-col>
+      </b-row>
+      <b-row class="about-info">
+        <b-col class="col-12 col-md-6 mb-1">
+          <h4 class="pb-2">
+            {{ $t('about.content[3].subtitle') }}
+          </h4>
+          <p>{{ $t('about.content[3].text') }}</p>
+        </b-col>
+      </b-row>
+      <b-row class="about-info">
+        <b-col class="col-12 col-md-6 mb-1">
+          <h4 class="pb-2">
+            {{ $t('about.content[4].subtitle') }}
+          </h4>
+          <p>{{ $t('about.content[4].text') }}</p>
+        </b-col>
+      </b-row>
+      <b-row class="about-info">
+        <b-col class="col-12 col-md-6 mb-1">
+          <h4 class="pb-2">
+            {{ $t('about.content[5].subtitle') }}
+          </h4>
+          <p>{{ $t('about.content[5].text') }}</p>
+        </b-col>
+      </b-row>
+      <b-row class="about-info">
+        <b-col class="col-12 col-md-6 mb-1">
+          <h4 class="pb-2">
+            {{ $t('about.content[6].subtitle') }}
+          </h4>
+          <p>{{ $t('about.content[6].text') }}</p>
         </b-col>
       </b-row>
     </b-container>
--- src/components/Cover.vue
+++ src/components/Cover.vue
@@ -5,12 +5,14 @@
   >
     <b-container>
       <b-row class="min-vh-100 w-100">
-        <b-col class="col-12 col-md-6 mt-auto cover-info">
+        <b-col class="col-12 col-md-6 cover-info">
           <img
             src="static/img/logo.png"
             alt="Bachillerato Popular Travesti - Trans Mocha Celis"
-            class="logo"
+            id="logo"
           >
+        </b-col>
+        <b-col class="col-12 col-md-6 mt-auto cover-info">
           <h2 class="mb-4">
             {{ $t('cover.title') }}
           </h2>
@@ -22,31 +24,31 @@
             {{ $t('donations.donate') }}
           </b-button>
         </b-col>
-        <b-col class="col-12 mt-auto">
-          <ul class="social-links mt-4">
-            <li>
-              <a
-                target="_blank"
-                href="https://www.facebook.com/facttic/"
-                title="Facebook"
-              ><font-awesome-icon :icon="['fab', 'facebook-square']" /><span class="sr-only">Facebook</span></a>
-            </li>
-            <li>
-              <a
-                target="_blank"
-                href="https://www.instagram.com/facttic.ar/"
-                title="Instagram"
-              ><font-awesome-icon :icon="['fab', 'instagram']" /><span class="sr-only">Instagram</span></a>
-            </li>
-            <li>
-              <a
-                target="_blank"
-                href="https://twitter.com/facttic"
-                title="Twitter"
-              ><font-awesome-icon :icon="['fab', 'twitter']" /><span class="sr-only">Twitter</span></a>
-            </li>
-          </ul>
-        </b-col>
+          <b-col class="col-12 col-md-6 mt-auto">
+            <ul class="social-links mt-4">
+              <li>
+                <a
+                  target="_blank"
+                  href="https://www.facebook.com/facttic/"
+                  title="Facebook"
+                ><font-awesome-icon :icon="['fab', 'facebook-square']" /><span class="sr-only">Facebook</span></a>
+              </li>
+              <li>
+                <a
+                  target="_blank"
+                  href="https://www.instagram.com/facttic.ar/"
+                  title="Instagram"
+                ><font-awesome-icon :icon="['fab', 'instagram']" /><span class="sr-only">Instagram</span></a>
+              </li>
+              <li>
+                <a
+                  target="_blank"
+                  href="https://twitter.com/facttic"
+                  title="Twitter"
+                ><font-awesome-icon :icon="['fab', 'twitter']" /><span class="sr-only">Twitter</span></a>
+              </li>
+            </ul>
+          </b-col>
       </b-row>
     </b-container>
   </div>
