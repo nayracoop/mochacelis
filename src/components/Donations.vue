22308fd47a5450b190ec48ee1493291026d9c84a
Martina <martina.colmenero@nayra.coop>
2020-05-13 11:42:43 -0300

    wip estilos

--- src/components/Donations.vue
+++ src/components/Donations.vue
@@ -9,14 +9,14 @@
         </b-col>
       </b-row>
       <b-row>
-         <b-col>
+         <b-col class="col-12 col-md-8">
           <p>{{ $t('donations.content[0]') }}</p>
           <p>{{ $t('donations.content[1]') }}</p>
           <p>{{ $t('donations.content[2]') }}</p>
          </b-col>
       </b-row>
       <b-row>
-        <b-col>
+        <b-col class="col-12 col-md-6 mb-2 donation-type">
         <h4>Donación única (dentro de Argentina)</h4>
         <b-button>{{ $t('donations.donate') }}</b-button>
         <b-button>{{ $t('donations.donate') }}</b-button>
@@ -24,7 +24,7 @@
         </b-col>        
       </b-row>
       <b-row>
-        <b-col>
+        <b-col class="col-12 col-md-6 mb-2 donation-type">
           <h4>Suscripción a débito automático (dentro de Argentina)</h4>
           <b-button>{{ $t('donations.donate') }}</b-button>
           <b-button>{{ $t('donations.donate') }}</b-button>
@@ -32,7 +32,7 @@
         </b-col>
       </b-row>
       <b-row>
-        <b-col>
+        <b-col class="col-12 col-md-6 mb-2 donation-type">
           <h4>Donar desde el exterior de Argentina</h4>
           <b-button>{{ $t('donations.donate') }}</b-button>
         </b-col>
