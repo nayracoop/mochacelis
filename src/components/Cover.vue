13ef61dfe9c95b5d9d4e0a3916c38a58a32af67a
Martina <martina.colmenero@nayra.coop>
2020-05-13 21:16:49 -0300

    wip cover design and content

--- src/components/Cover.vue
+++ src/components/Cover.vue
@@ -5,50 +5,56 @@
   >
     <b-container>
       <b-row class="min-vh-100 w-100">
-        <b-col class="col-12 col-md-6 cover-info">
+        <b-col class="col-12 col-md-6 cover-logo">
           <img
             src="static/img/logo.png"
             alt="Bachillerato Popular Travesti - Trans Mocha Celis"
             id="logo"
           >
         </b-col>
-        <b-col class="col-12 col-md-6 mt-auto cover-info">
+        <b-col class="col-12 col-md-6 cover-info">
           <h2 class="mb-4">
             {{ $t('cover.title') }}
           </h2>
-          <p class="mb-4" v-html="$t('cover.content')"></p>
+          <h1 class="mb-4">
+            {{ $t('cover.subtitle') }}
+          </h1>
+          <p>
+            {{$t('cover.content[0]')}}
+          </p>
+          <p>
+            {{$t('cover.content[1]')}}
+          </p>
           <b-button href="#contact">
             {{ $t('cover.btn-text') }}
           </b-button>
           <b-button href="#donations">
             {{ $t('donations.donate') }}
           </b-button>
+          <ul class="social-links">
+            <li>
+              <a
+                target="_blank"
+                href="https://www.facebook.com/BachiTransMocha/"
+                title="Facebook"
+              ><font-awesome-icon :icon="['fab', 'facebook-square']" /></a>
+            </li>
+            <li>
+              <a
+                target="_blank"
+                href="https://www.instagram.com/mochacelis/"
+                title="Instagram"
+              ><font-awesome-icon :icon="['fab', 'instagram']" /></a>
+            </li>
+            <li>
+              <a
+                target="_blank"
+                href="https://twitter.com/mochacelis"
+                title="Twitter"
+              ><font-awesome-icon :icon="['fab', 'twitter']" /></a>
+            </li>
+          </ul>
         </b-col>
-          <b-col class="col-12 col-md-6 mt-auto">
-            <ul class="social-links mt-4">
-              <li>
-                <a
-                  target="_blank"
-                  href="https://www.facebook.com/facttic/"
-                  title="Facebook"
-                ><font-awesome-icon :icon="['fab', 'facebook-square']" /><span class="sr-only">Facebook</span></a>
-              </li>
-              <li>
-                <a
-                  target="_blank"
-                  href="https://www.instagram.com/facttic.ar/"
-                  title="Instagram"
-                ><font-awesome-icon :icon="['fab', 'instagram']" /><span class="sr-only">Instagram</span></a>
-              </li>
-              <li>
-                <a
-                  target="_blank"
-                  href="https://twitter.com/facttic"
-                  title="Twitter"
-                ><font-awesome-icon :icon="['fab', 'twitter']" /><span class="sr-only">Twitter</span></a>
-              </li>
-            </ul>
-          </b-col>
       </b-row>
     </b-container>
   </div>
