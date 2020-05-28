7712c68bba0fc09e125b5ac557b61e10f6071f60
Martina Colmenero <martina.colmenero@nayra.coop>
2020-05-28 20:27:29 -0300

    estilo cover + prod

--- src/components/Cover.vue
+++ src/components/Cover.vue
@@ -1,33 +1,33 @@
 <template>
   <div
     id="cover"
-    class="min-vh-100 d-flex flex-column align-items-center align-content-center"
+    class="min-vh-60 d-flex flex-column align-items-center align-content-center"
   >
     <b-container class="min-vh-100 w-100 cover-info">
       <b-row>
-        <b-col class="col-12 mb-5" >
+        <b-col class="col-12" >
           <h1> {{ $t('cover.title') }} </h1>
         </b-col>
       </b-row>
       <b-row>
         <b-col class="col-12 col-md-6">
           <img
-            src="static/img/logo.png"
-            alt="Bachillerato Popular Travesti - Trans Mocha Celis"
+            src="static/img/cover-image.png"
+            alt="Bachillerato Popular Travesti-Trans Mocha Celis"
             class="col-12 cover-image"
           >
           <b-button class="cover-button" href="mailto: preceptoria@bachilleratomochacelis.edu.ar">
-            {{ $t('cover.btn-text') }}
+            {{ $t('cover.btn-text-1') }}
           </b-button>
         </b-col>
         <b-col class="col-12 col-md-6">
           <img
-            src="static/img/logo.png"
-            alt="Bachillerato Popular Travesti - Trans Mocha Celis"
+            src="static/img/cover-image.png"
+            alt="Bachillerato Popular Travesti-Trans Mocha Celis"
             class="col-12 cover-image"
           >
           <b-button class="cover-button" href="#donations">
-            {{ $t('donations.donate') }}
+            {{ $t('cover.btn-text-2') }}
           </b-button>
         </b-col>
       </b-row>
