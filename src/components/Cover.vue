d0da022542509d500c5b8a5071b0f267f732cf2f
romi gomez <romina.gomez@nayra.coop>
2020-05-28 23:57:36 -0300

    cover fixes desktop and mobile

--- src/components/Cover.vue
+++ src/components/Cover.vue
@@ -1,43 +1,34 @@
 <template>
-  <div
-    id="cover"
-    class="min-vh-60 d-flex flex-column align-items-center align-content-center"
-  >
-    <b-container class="min-vh-100 w-100 cover-info">
+  <div id="cover" class>
+    <b-container class="cover-info">
       <b-row>
-        <b-col class="col-12" >
-          <h1> {{ $t('cover.title') }} </h1>
+        <b-col class="col-12">
+          <h1>{{ $t('cover.title') }}</h1>
         </b-col>
       </b-row>
       <b-row>
-        <b-col class="col-12 col-md-6">
+        <b-row class="cover-image">
           <img
-            src="static/img/cover-image-1.png"
+            src="static/img/cover-image-3.png"
             alt="Bachillerato Popular Travesti-Trans Mocha Celis"
-            class="col-12 cover-image"
-          >
-          <b-button class="cover-button" href="mailto: preceptoria@bachilleratomochacelis.edu.ar">
-            {{ $t('cover.btn-text-1') }}
-          </b-button>
-        </b-col>
-        <b-col class="col-12 col-md-6">
-          <img
-            src="static/img/cover-image-2.png"
-            alt="Bachillerato Popular Travesti-Trans Mocha Celis"
-            class="col-12 cover-image"
-          >
-          <b-button class="cover-button" href="#donations">
-            {{ $t('cover.btn-text-2') }}
-          </b-button>
-        </b-col>
+          />
+        </b-row>
+        <b-row class="cover-buttons">
+          <b-col class="col-12 col-md-6">
+            <b-button
+              class="cover-button"
+              href="mailto: preceptoria@bachilleratomochacelis.edu.ar"
+            >{{ $t('cover.btn-text-1') }}</b-button>
+          </b-col>
+          <b-col class="col-12 col-md-6">
+            <b-button class="cover-button" href="#donations">{{ $t('cover.btn-text-2') }}</b-button>
+          </b-col>
+        </b-row>
       </b-row>
     </b-container>
   </div>
 </template>
 
 <script>
-
-export default {
-
-}
+export default {};
 </script>
