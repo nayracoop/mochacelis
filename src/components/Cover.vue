bf8aaf66542d1464031f00e7cc2cf78dca836f04
unknown <tobias.irigoyen@nayra.coop>
2020-05-29 14:19:47 -0300

    Change cover style. Hide cover heading. Set target blank to footer links

--- src/components/Cover.vue
+++ src/components/Cover.vue
@@ -3,7 +3,7 @@
     <b-container class="cover-info">
       <b-row>
         <b-col class="col-12">
-          <h1>{{ $t('cover.title') }}</h1>
+          <h1 class="sr-only">{{ $t('cover.title') }}</h1>
         </b-col>
       </b-row>
       <b-row>
@@ -16,12 +16,12 @@
         <b-row class="cover-buttons">
           <b-col class="col-12 col-md-6">
             <b-button
-              class="cover-button"
+              class="cover-button b-purple"
               href="mailto: preceptoria@bachilleratomochacelis.edu.ar"
             >{{ $t('cover.btn-text-1') }}</b-button>
           </b-col>
           <b-col class="col-12 col-md-6">
-            <b-button class="cover-button" href="#donations">{{ $t('cover.btn-text-2') }}</b-button>
+            <b-button class="cover-button b-orange" href="#donations">{{ $t('cover.btn-text-2') }}</b-button>
           </b-col>
         </b-row>
       </b-row>
--- src/components/RealFooter.vue
+++ src/components/RealFooter.vue
@@ -5,10 +5,10 @@
       <b-row>
         <b-col class="col-12">
           <div class="real-footer text-center">
-            <small class="made-with mt-3"><a href="https://nayra.coop" v-html="$t('footer.signature')"></a></small>
+            <small class="made-with mt-3"><a target="_blank" href="https://nayra.coop" v-html="$t('footer.signature')"></a></small>
             <div class="licence-cont mt-2">
-              <small class="licence">{{ $t('footer.licence') }} <a href="http://creativecommons.org/licenses/by-sa/4.0/">{{ $t('footer.licenceLinkTitle') }}.</a></small>
-              <a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/">
+              <small class="licence">{{ $t('footer.licence') }} <a target="_blank" href="http://creativecommons.org/licenses/by-sa/4.0/">{{ $t('footer.licenceLinkTitle') }}.</a></small>
+              <a target="_blank" rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/">
                 <img class="mt-3" alt="Licencia Creative Commons" src="static/img/licence.png">
               </a>
             </div>
