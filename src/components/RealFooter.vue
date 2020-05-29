53057245cd8e29a91002eefe543bf5afc1ad0c28
unknown <tobias.irigoyen@nayra.coop>
2020-05-29 13:49:27 -0300

    Footer fix. Cover image replacement. Header links colors

--- src/components/RealFooter.vue
+++ src/components/RealFooter.vue
@@ -1,26 +1,21 @@
 <template>
 <div id="bottom">
     <hr>
+    <b-container>
       <b-row>
         <b-col class="col-12">
           <div class="real-footer text-center">
-            <small class="mb-3 made-with mt-3" v-html="$t('footer.signature')"></small>
-            <div class="licence-cont">
-              <a
-                rel="license"
-                href="http://creativecommons.org/licenses/by-sa/4.0/"
-              >
-                <img
-                  class="mb-2"
-                  alt="Licencia Creative Commons"
-                  src="static/img/licence.png"
-                >
+            <small class="made-with mt-3"><a href="https://nayra.coop" v-html="$t('footer.signature')"></a></small>
+            <div class="licence-cont mt-2">
+              <small class="licence">{{ $t('footer.licence') }} <a href="http://creativecommons.org/licenses/by-sa/4.0/">{{ $t('footer.licenceLinkTitle') }}.</a></small>
+              <a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/">
+                <img class="mt-3" alt="Licencia Creative Commons" src="static/img/licence.png">
               </a>
-              <small class="licence">{{ $t('footer.licence') }} <a href="http://creativecommons.org/licenses/by-sa/4.0/">{{ $t('footer.licenceLinkTitle') }}</a>.</small>
             </div>
           </div>
         </b-col>
       </b-row>
+    </b-container>
 </div>
 </template>
 <script>
